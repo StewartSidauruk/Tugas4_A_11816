2a17bc91952ea5e68956a057e5bd04d94fd464b0
StewartSidauruk <220711816@students.uajy.ac.id>
2024-11-07 17:49:52 +0700

    Update Tugas_Modul4_A_Stiwat_11816.py

--- Tugas_Modul4_A_Stiwat_11816.py
+++ Tugas_Modul4_A_Stiwat_11816.py
@@ -76,9 +76,9 @@ if uploaded_file is not None:
         })
 
         cluster_method = [
-        ("KMeans_model", models["KMeans_model"], "KMeans Clustering", px.colors. sequential.Cividis),
-        ("AGG_model", models["AGG_model"], "Agglomerative Clustering", px.colors. sequential.Mint),
-        ("DBSCAN_model", models["DBSCAN_model"], "DBSCAN Clustering", px.colors. sequential.Plasma)
+        ("KMeans_model", models["KMeans_model"], "KMeans Clustering", px.colors.sequential.Cividis),
+        ("AGG_model", models["AGG_model"], "Agglomerative Clustering", px.colors.sequential.Mint),
+        ("DBSCAN_model", models["DBSCAN_model"], "DBSCAN Clustering", px.colors.sequential.Plasma)
         ]
 
         col1, col2, col3 = st.columns(3)
